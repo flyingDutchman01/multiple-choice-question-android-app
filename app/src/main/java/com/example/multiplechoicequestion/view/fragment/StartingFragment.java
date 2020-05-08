e47359ff61aaec340878e935984ac88e793ee0cc
Sahas Prajapati <b10prajapati@gmail.com>
2020-05-08 11:09:55 +0545

    error resolved

--- app/src/main/java/com/example/multiplechoicequestion/view/fragment/StartingFragment.java
+++ app/src/main/java/com/example/multiplechoicequestion/view/fragment/StartingFragment.java
@@ -1,14 +1,13 @@
 package com.example.multiplechoicequestion.view.fragment;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.example.multiplechoicequestion.R;
 
@@ -32,12 +31,13 @@ public class StartingFragment extends Fragment {
         // Inflate the layout for this fragment
       View v =  inflater.inflate(R.layout.fragment_starting, container, false);
       button = v.findViewById(R.id.first_button);
-
+        fragmentManager = getParentFragmentManager();
       button.setOnClickListener(new View.OnClickListener() {
           @Override
           public void onClick(View v) {
+
               fragmentManager.beginTransaction()
-                      .replace(R.id.startingFragment, categoryFragment)
+                      .replace(R.id.container, CategoryFragment.newInstance())
                       .addToBackStack("Stack")
                       .commit();
           }
